cbedf612f43691fd03e55c8ea1f07eee8c9e86be
Annebula77 <goharikv@gmail.com>
2023-10-26 23:16:53 +0300

    initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [TypeOrmModule.forRoot()],
+  controllers: [AppController],
+  providers: [AppService],
+})
+
+// eslint-disable-next-line prettier/prettier
+export class AppModule { }
